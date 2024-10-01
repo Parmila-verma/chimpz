e1108ee64964d4a78625fb3d90c073ef41545213
unknown <you@example.com>
2024-10-01 16:09:41 +0530

    first commit

---
+++ assets/css/js/2nd.js
@@ -0,0 +1,15 @@
+const menuIcon = document.querySelector(".menu-icon");
+const list = document.querySelector(".list");
+
+menuIcon.addEventListener("click", () => {
+    list.classList.toggle("show");
+    if (list.classList.contains("show")) {
+        menuIcon.innerHTML = `
+            <ion-icon name="close-outline"  class="mobile-icons"></ion-icon>
+        `;
+    } else {
+        menuIcon.innerHTML = `
+         <ion-icon name="menu-outline"  class="mobile-icons"></ion-icon>
+        `;
+    }
+});
